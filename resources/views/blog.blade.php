9a1f89acc54b3ed8634654227df2a0d548180a76
daudhiyaa <daud.dhiya31@gmail.com>
2024-09-23 22:52:42 +0700

    feat: finishing 5th video (blade component)

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:header>{{ $header }}</x-slot:header>
+    {{-- slot content --}}
+    <h3>Blog Page</h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home', 'header' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About', 'header' => 'About Page']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog', 'header' => 'Blog Page']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact', 'header' => 'Contact Page']);
 });
